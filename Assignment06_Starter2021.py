a95f6b8143d54e344b08711a3e210e37bcf0a33a
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2021-11-26 21:34:35 -0800

    Update Assignment06_Starter2021.py
    Updated script to follow list, dictionary format.

--- Assignment06_Starter2021.py
+++ Assignment06_Starter2021.py
@@ -6,13 +6,16 @@
 #              Add the each dictionary "row" to a python list "table"
 # ChangeLog (Who,When,What):
 # RRoot,1.1.2030,Created started script
-# KWONG,11.23.2021,Modified code to complete assignment 06
+# KWONG, 11.22.2021, Modified code to complete assignment 06
+# KWONG, 11.26.2021, Modified code to correctly complete assignment 06 in list format
 # ---------------------------------------------------------------------------- #
 
 # Data ---------------------------------------------------------------------- #
 # Declare variables and constants
 file_name_str = "ToDoFile.txt"  # The name of the data file
 file_obj = None  # An object that represents a file
+row_dic = {}  # A row of data separated into elements of a dictionary {Task,Priority}
+table_lst = []  # A list that acts as a 'table' of rows
 choice_str = ""  # Captures the user option selection
 
 
@@ -28,59 +31,63 @@ class Processor:
         :param list_of_rows: (list) you want filled with file data:
         :return: (list) of dictionary rows
         """
-        try:
-            strFile = open(file_name_str, 'r')
-            for row in strFile:
-                lstRow = row.split(', ')
-                task = lstRow[0]
-                priority = lstRow[1].strip()
-                list_of_rows[task] = priority
-            for task, priority in dicRow.items():
-                print(task, ', ', priority)
-        except:
-            print('No data found!')
-            list_of_rows = {}
+        list_of_rows.clear()  # clear current data
+        file = open(file_name, "r")
+        for line in file:
+            task, priority = line.split(",")
+            row = {"Task": task.strip(), "Priority": priority.strip()}
+            list_of_rows.append(row)
+        file.close()
         return list_of_rows
 
     @staticmethod
     def add_data_to_list(task, priority, list_of_rows):
-        """ Adds task and priority
+        """ Adds new data into list of dictionary rows
 
-        :param task: (string) you want to fill dictionary with task
-        :param priority (string) you want to fill dictionary with priority
-        :param list_of_rows (dictionary)
-        :return: (dictionary) of task and priority
+        :param task: (string) you want to add to list
+        :param priority: (list) you want to attach to task
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
         """
-        list_of_rows[task] = priority # sets task to priority in dictionary
+        row_dic = {"Task": task, "Priority": priority}
+        list_of_rows.append(row_dic)
+        print("\n Okay,", task, "added!")
         return list_of_rows
 
     @staticmethod
     def remove_data_from_list(task, list_of_rows):
-        """ Removes data from dictionary
-        :param task: (string) which task to remove from dictionary
-        :param list_of_rows (dictionary) you want to fill dictionary with priority
-        :return: (dictionary) of task and priority
+        """ Removes data from list of dictionary rows
+
+        :param task: (string) you want to remove from list
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
         """
-        del list_of_rows[task] # deletes task in dictionary
-        print('\nOkay, I have deleted', task)
+        row_num = 0
+        for row in list_of_rows:
+            task_check, priority_check = dict(row).values()
+            if task.lower() == task_check.lower():
+                del list_of_rows[row_num]
+                row_num += 1
+                print("\n Okay,", task, "deleted!")
+            else:
+                row_num += 1
         return list_of_rows
 
     @staticmethod
     def write_data_to_file(file_name, list_of_rows):
-        """ Removes task from dictionary
-        :param file_name: (string) which file to write the dictionary to
-        :param list_of_rows (dictionary) to write into file
-        :return: (dictionary) of task and priority
+        """ Removes data from list of dictionary rows
+
+        :param file_name: (string) with name of file:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
         """
-        with open(file_name_str, 'w') as f: # opens file and writes formatted task and priority into file
-            for task, priority in list_of_rows.items():
-                format_dictionary = '{tname}, {pname}\n'.format(tname = task, pname = priority)
-                f.write(str(format_dictionary))
-        #close file
-        f.close()
-        # print below message
-        print('Data saved!')
+        file_obj = open(file_name_str, "w")
+        for row_dic in list_of_rows:
+            file_obj.write(row_dic["Task"] + "," + row_dic["Priority"] + "\n")
+        print("File saved.")
+        file_obj.close()
         return list_of_rows
+
 
 # Presentation (Input/Output)  -------------------------------------------- #
 
@@ -106,7 +113,7 @@ class IO:
     @staticmethod
     def input_menu_choice():
         """ Gets the menu choice from a user
-        :param: None
+
         :return: string
         """
         choice = str(input("Which option would you like to perform? [1 to 5] - ")).strip()
@@ -121,65 +128,59 @@ class IO:
         :return: nothing
         """
         print("******* The current tasks ToDo are: *******")
-        if list_of_rows != {}: # if dictionary is not empty, format and print out the task and priority
-            for task, priority in list_of_rows.items():
-                print(task,", ", priority)
+        for row in list_of_rows:
+            print(row["Task"] + " (" + row["Priority"] + ")")
         print("*******************************************")
         print()  # Add an extra line for looks
 
     @staticmethod
     def input_new_task_and_priority():
-        """ Requesting task and priority from user
+        """ Removes data from list of dictionary rows
 
-        :param: None
-        :return: task, priority
+        :param task: (string) data you want to add to file:
+        :param priority: (string) you want to attach to task:
+        :return: (string) of both datapoints
         """
-        task = str(input("Which task would you like to add? ")) # requesting which task to add
-        if task not in list_of_rows: # looping through dictionary and checking for task's existance
-            priority = str(input("What is the priority of the task? ")) # requesting priority of task if there
-            print("\nOkay", task, "has been inputted")
-        else:
-            print("\n", task, "is already in the list!")
-            priority = None # setting priority is None
+        task = str(input("What task would you like to add? "))
+        priority = str(input("What is the priority of this task? "))
         return task, priority
 
     @staticmethod
     def input_task_to_remove():
-        """ Requesting task and priority from user
+        """ Removes data from list of dictionary rows
 
-        :param: None
-        :return: task (string) to be removed
+        :param task: (string) data you want to remove from file:
+        :return: (string)
         """
-        task = str(input("Which task would you like to remove? ")) # requests task
-        if task not in list_of_rows: # loops through dictionary to check if task in dictionary
-            print("\n", task, "does not exist!")
+        task = str(input("Which task would you like to remove? "))
         return task
+
 
 # Main Body of Script  ------------------------------------------------------ #
 
 # Step 1 - When the program starts, Load data from ToDoFile.txt.
-list_of_rows = Processor.read_data_from_file(file_name_str, table_lst)  # read file data
+Processor.read_data_from_file(file_name_str, table_lst)  # read file data
 
 # Step 2 - Display a menu of choices to the user
 while (True):
     # Step 3 Show current data
-    IO.output_current_tasks_in_list(list_of_rows)  # Show current data in the list/table
+    IO.output_current_tasks_in_list(table_lst)  # Show current data in the list/table
     IO.output_menu_tasks()  # Shows menu
     choice_str = IO.input_menu_choice()  # Get menu option
 
     # Step 4 - Process user's menu choice
     if choice_str.strip() == '1':  # Add a new Task
-        task, priority = IO.input_new_task_and_priority() # set task and priority to results of function
-        list_of_rows = Processor.add_data_to_list(task, priority, list_of_rows) # set list_of_rows to results of function
+        task, priority = IO.input_new_task_and_priority() # requests for data from user
+        table_lst = Processor.add_data_to_list(task, priority, table_lst) # adds requested data into list
         continue  # to show the menu
 
     elif choice_str == '2':  # Remove an existing Task
-        task = IO.input_task_to_remove() # which task to remove from dictionary
-        list_of_rows = Processor.remove_data_from_list(task, list_of_rows) # set list of rows to results of function
+        task = IO.input_task_to_remove() # requests for data from user
+        table_lst = Processor.remove_data_from_list(task, table_lst) # removes data from list if found
         continue  # to show the menu
 
     elif choice_str == '3':  # Save Data to File
-        Processor.write_data_to_file(file_name_str, list_of_rows) # execute function to save data
+        table_lst = Processor.write_data_to_file(file_name_str, table_lst) # saves data to file
         continue  # to show the menu
 
     elif choice_str == '4':  # Exit Program
